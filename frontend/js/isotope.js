eee0cb2628688b3a595c1d397568f8aac536e232
Vitalii Khlystun <v.khlystun01@gmail.com>
2017-12-25 11:46:52 +0200

    q

--- frontend/js/isotope.js
+++ frontend/js/isotope.js
@@ -1,17 +1,85 @@
-jQuery(function($) {
-    $('.jobs_list').isotope({
-        itemSelector: '.jobs_item',
-        layoutMode: 'fitRows',
-    })
-    $('.jobs_cites div,.jobs_location div').on('click', function() {
-        $('.jobs_cites div,.jobs_location div').removeClass('active-filter')
-        $(this).addClass('active-filter');
+// jQuery(function($) {
+//     $('.jobs_list').isotope({
+//         // hiddenStyle: {
+//         //     opacity: 0
+//         // },
+//         // visibleStyle: {
+//         //     opacity: 1
+//         // },
+//         // stagger: 30,
+//         itemSelector: '.jobs_item',
+//         layoutMode: 'fitRows',
+//     })
+//     $('.jobs_cites div,.jobs_location div').on('click', function() {
+//         $('.jobs_cites div,.jobs_location div').removeClass('active-filter')
+//         $(this).addClass('active-filter');
 
-        var selector = $(this).attr('data-filter');
-        $('.jobs_list').isotope({
-            filter: selector
+//         var selector = $(this).attr('data-filter');
+//         $('.jobs_list').isotope({
+//             filter: selector
 
+//         });
+//         return false;
+//     })
+// });
+$(document).ready(function() {
+    // get the action filter option item on page load
+    var $filterType1 = $('#filter-cites li.active-button a').attr('class');
+    var $filterType2 = $('#filter-location li.active-button a').attr('class');
+    // get and assign the ourHolder element to the
+    // $holder varible for use later
+    var $holder = $('.jobs_list');
+
+    // clone all items within the pre-assigned $holder element
+    var $data = $holder.clone();
+    // attempt to call Quicksand when a filter option
+    // item is clicked
+    $('#filter-cites li a').click(function(e) {
+        // reset the active class on all the buttons
+        $('#filter-cites li').removeClass('active-button');
+        // assign the class of the clicked filter option
+        // element to our $filterType variable
+        var $filterType1 = $(this).attr('class');
+        $(this).parent().addClass('active-button');
+        if ($filterType1 == 'all') {
+            // assign all li items to the $filteredData var when
+            // the 'All' filter option is clicked
+            var $filteredData = $data.find('li');
+        } else {
+            // find all li elements that have our required $filterType
+            // values for the data-type element
+            var $filteredData = $data.find('li[data-type=' + $filterType1 + ']');
+        }
+
+        // call quicksand and assign transition parameters
+        $holder.quicksand($filteredData, {
+            duration: 800,
+            easing: 'easeInOutQuad'
         });
         return false;
-    })
+    });
+    $('#filter-location li a').click(function(e) {
+        // reset the active class on all the buttons
+        $('#filter-location li').removeClass('active-button');
+        // assign the class of the clicked filter option
+        // element to our $filterType variable
+        var $filterType2 = $(this).attr('class');
+        $(this).parent().addClass('active-button');
+        if ($filterType2 == 'all') {
+            // assign all li items to the $filteredData var when
+            // the 'All' filter option is clicked
+            var $filteredData = $data.find('li');
+        } else {
+            // find all li elements that have our required $filterType
+            // values for the data-type element
+            var $filteredData = $data.find('li[data-location=' + $filterType2 + ']');
+        }
+
+        // call quicksand and assign transition parameters
+        $holder.quicksand($filteredData, {
+            duration: 800,
+            easing: 'easeInOutQuad'
+        });
+        return false;
+    });
 });
--- frontend/js/jobs.js
+++ frontend/js/jobs.js
@@ -4,41 +4,99 @@ jQuery(function($) {
     var $itemContent = $item.find('.jobs_item-content');
 
     $item.on('click', function() {
-        var $this = $(this)
-        var $itemContent = $this.find('.jobs_item-content');
-        var $heightBlock = $this.find('.description_wrapper').outerHeight()
+            console.log('hi')
+            var $this = $(this)
+            var $itemContent = $this.find('.jobs_item-content');
+            var $heightBlock = $this.find('.description_wrapper').outerHeight();
+            var $list = $('.jobs_list')
+                // var $heightSection = $list.outerHeight();
 
-        if (!$item.find('.jobs_item-content').hasClass('open-description')) {
-            $item.find('.jobs_item-content, .description').css('height', '0')
-            $itemContent.addClass('open-description')
-            $item.removeClass('active-item')
-            $this.addClass('active-item')
-            $this.find('.jobs_item-content, .description').css('height', $heightBlock)
-        } else {
-            $item.find('.jobs_item-content').removeClass('open-description')
-            $this.find('.jobs_item-content, .description').css('height', '0')
-            $itemContent.removeClass('open-description');
-            $this.removeClass('active-item')
-        }
+            if (!$item.find('.jobs_item-content').hasClass('open-description')) {
+                $list.css('height', '100%')
+                $item.find('.jobs_item-content, .description').css('height', '0')
+                $itemContent.addClass('open-description')
+                $item.removeClass('active-item')
+                $this.addClass('active-item')
+                $this.find('.jobs_item-content, .description').css('height', $heightBlock)
+                    // $list.css('height', $heightSection + $heightBlock)
+            } else {
+                $list.css('height', 'auto')
+                $item.find('.jobs_item-content').removeClass('open-description')
+                $this.find('.jobs_item-content, .description').css('height', '0')
+                $itemContent.removeClass('open-description');
+                $this.removeClass('active-item')
+                    // $list.css('height', $heightSection - $heightBlock)
+            }
 
-        // $this.on('mouseout', function() {
-        //     $itemContent.addClass('open-description');
-        // })
-    })
-    $item.on('mouseover', function() {
-        $itemContent.removeClass('open-description');
+            // $this.on('mouseout', function() {
+            //     $itemContent.addClass('open-description');
+            // })
 
-    })
+        })
+        // $item.on('mouseover', function() {
+        //     $itemContent.removeClass('open-description');
+
+    // })
 
 
 
     $('body').on('click', function(e) {
-
+        // $list.css('height', $heightSection)
         var target = $(e.target);
-
+        console.log(target)
         if (target.is(".row, .big-title")) {
             $item.removeClass('active-item')
             $item.find('.jobs_item-content, .description').css('height', '0')
         }
     })
 });
+
+
+// jQuery(function($) {
+//     var $list = $('.jobs_list');
+//     var $item = $list.find('.jobs_item');
+//     var $itemContent = $item.find('.jobs_item-content');
+
+//     $item.on('click', function() {
+//             var $this = $(this)
+//             var $itemContent = $this.find('.jobs_item-content');
+//             var $heightBlock = $this.find('.description_wrapper').outerHeight()
+//             var $heightSection = $list.outerHeight();
+
+//             if (!$item.find('.jobs_item-content').hasClass('open-description')) {
+//                 $item.find('.jobs_item-content, .description').css('height', '0')
+//                 $itemContent.addClass('open-description')
+//                 $item.removeClass('active-item')
+//                 $this.addClass('active-item')
+//                 $this.find('.jobs_item-content, .description').css('height', $heightBlock)
+//                 $list.css('height', $heightSection + $heightBlock)
+//             } else {
+//                 $item.find('.jobs_item-content').removeClass('open-description')
+//                 $this.find('.jobs_item-content, .description').css('height', '0')
+//                 $itemContent.removeClass('open-description');
+//                 $this.removeClass('active-item')
+//                 $list.css('height', $heightSection - $heightBlock)
+//             }
+
+//             // $this.on('mouseout', function() {
+//             //     $itemContent.addClass('open-description');
+//             // })
+
+//         })
+//         // $item.on('mouseover', function() {
+//         //     $itemContent.removeClass('open-description');
+
+//     // })
+
+
+
+//     $('body').on('click', function(e) {
+//         // $list.css('height', $heightSection)
+//         var target = $(e.target);
+
+//         if (target.is(".row, .big-title")) {
+//             $item.removeClass('active-item')
+//             $item.find('.jobs_item-content, .description').css('height', '0')
+//         }
+//     })
+// });
